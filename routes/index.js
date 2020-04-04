00d143db70dd360915b75eea6c0595fe4f355908
arungowda-p <33170786+arungowda-p@users.noreply.github.com>
2020-04-04 12:24:25 +0530

    Procfile

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/timesheet', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('timesheet', { title: 'TIMESHEET REMINDER POC' });
 });
 
