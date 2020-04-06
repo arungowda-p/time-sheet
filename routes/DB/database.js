1d64bfd69038c6b5f45bfc10056d63310ddd3d45
arungowda-p <33170786+arungowda-p@users.noreply.github.com>
2020-04-06 19:29:46 +0530

    database

---
+++ routes/DB/database.js
@@ -0,0 +1,92 @@
+var fs = require("fs");
+
+module.exports = {
+  create: function (name) {
+    try {
+      var path = __dirname + "/base/";
+      console.log(path);
+      if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+      }
+      var content = [];
+      fs.access(path + name + ".json", (err) => {
+        if (!err) {
+          console.log("Table already exsits");
+        } else {
+          fs.writeFile(
+            path + name + ".json",
+            JSON.stringify(content),
+            function (err, file) {
+              if (err) throw err;
+              console.log("created");
+            }
+          );
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  insert: function (table, data) {
+    try {
+      var path = __dirname + "/base/";
+      if (!fs.existsSync(path)) {
+        console.log("Create the Table to Insert");
+      }
+      fs.access(path + table + ".json", (err) => {
+        if (err) {
+          console.log("Create a table to Intert");
+        } else {
+          var rawcontent = fs.readFileSync(path + table + ".json");
+          var content = JSON.parse(rawcontent);
+          content.push(data);
+          fs.writeFile(
+            path + table + ".json",
+            JSON.stringify(content),
+            (err, file) => {
+              if (err) throw err;
+              console.log("Table Updated");
+            }
+          );
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  find: (table, key, value,callback) => {
+    var keyVal;
+    var ret;
+    try {
+      var path = __dirname + "/base/";
+      if (!fs.existsSync(path)) {
+        console.log("Create the Table to Perform Action");
+      }
+      fs.access(path + table + ".json", (err) => {
+        if (err) {
+          console.log("Table not Found");
+        } else {
+          var rawcontent = fs.readFileSync(path + table + ".json");
+          var content = JSON.parse(rawcontent);
+          for (var i = 0; i < content.length; i++) {
+            console.log(content[i][key] + "       " + value);
+            if (content[i][key] === value) {
+              keyVal = content[i];
+            }
+          }
+          if (keyVal != undefined && keyVal != null) {
+            ret = keyVal;
+          } else {
+            console.log("Record not found");
+            ret = "null";
+          }
+          callback(ret);
+        }
+      });      
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
--- routes/server/add.js
+++ routes/server/add.js
@@ -1,7 +1,19 @@
+var db = require('../DB/database')
+
 module.exports = function(router){
 /* GET home page. */
 router.post('/add/:emp', function(req, res) {
- console.log(req.body)
- res.send(200)
+ //console.log(req.body)
+ //db.create('empe')
+ //db.insert('empe', req.body)
+ let data;
+db.find('empe', 'id', '168188',(val) => {
+   data = val;
+   console.log(data+"uuuuuuuuuuuuuuuuuuu")
+})
+ setTimeout(() => {
+    console.log(data+" 5524555")
+    res.send(data)
+ }, 500);
 });
 }
